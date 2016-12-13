6c22b10b685448d033105c473b181b792261c9ac
Joshua Rinard <jrinard@hotmail.com>
2016-12-13 12:02:52 -0800

    adds questions functionality

--- app.rb
+++ app.rb
@@ -15,6 +15,11 @@ get('/surveys') do
   erb(:survey_list)
 end
 
+get('/surveys/:id') do
+  @survey = Survey.find(params.fetch("id").to_i)
+  erb(:survey_detail)
+end
+
 post('/surveys') do
   @survey = Survey.new({:name => params.fetch('new-survey-name')})
   if @survey.save
@@ -22,4 +27,11 @@ post('/surveys') do
   else
     erb(:errors)
   end
+end
+
+#going back to individual survey page
+post('/questions') do
+  survey_id = params.fetch('new-question-survey-id').to_i
+  @question = Question.create({:text => params.fetch('new-question-text'), :survey_id => survey_id})
+  redirect "/surveys/#{survey_id}"
 end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -6,18 +6,35 @@ set(:show_exceptions, false)
 
  describe('Survey Routes', {:type => :feature}) do
    describe('Create Survey') do
-     it "allows you to create a survey" do
+     it "allows user to create a survey" do
        visit('/surveys')
        fill_in('new-survey-name', :with => "New Survey")
        click_button('Create Survey')
-       expect(page).to have_content('New Survey')
+       expect(page).to have_content 'New Survey'
      end
      it "shows an error page if validation fails" do
        visit('/surveys')
        fill_in('new-survey-name', :with => "")
        click_button('Create Survey')
-       expect(page).to have_content('Error')
-
+       expect(page).to have_content 'Error'
      end
    end
+   describe('show a survey') do
+     it "allows user to view individual survey" do
+       survey = Survey.create({:name => "Survey"})
+       visit("/surveys/#{survey.id}")
+       expect(page).to have_content 'Survey'
+     end
+   end
+   describe('add questions to survey') do
+     it "allows user to add a question to survey" do
+       survey = Survey.create({:name => "Survey"})
+       visit("/surveys/#{survey.id}")
+       fill_in('new-question-text', :with => "question 1")
+       click_button('Add Question')
+       expect(page).to have_content 'question 1'
+     end
+   end
+
+
  end
